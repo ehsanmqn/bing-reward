94bd2579bc9bc77ad46a786110956d1b8f72c20a
Ehsan <spin313@gmail.com>
2022-10-24 15:54:01 +0330

    Changes in openconnect

--- openconnect.sh
+++ openconnect.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "1195" | openconnect nls.lkserver.info:2087 --user=like53007100 --servercert=pin-sha256:2VAgAdd6ZRQYCmoSttHF3XxxmQiCgZU/LD/YSZmQf54 --passwd-on->
+echo "1195" | openconnect nls.lkserver.info:2087 --user=USER --servercert=pin-sha256:2VAgAdd6ZRQYCmoSttHF3XxxmQiCgZU/LD/YSZmQf54 --passwd-on->
